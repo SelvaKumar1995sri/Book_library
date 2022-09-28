5aea360b975f3c046116f8f50ccf380a62815a72
selva kumar <selvakumar1995sri@gmail.com>
2022-09-28 16:40:36 +0530

    procfile created for deploy and changes on root

--- app/bookmanager.py
+++ app/bookmanager.py
@@ -8,6 +8,7 @@ from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
